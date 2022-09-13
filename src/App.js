e2332955a1d57e718b2d3691c6b9251738df5772
Zubaer Alam <zobaeralam@Iut-dhaka.edu>
2022-09-13 08:59:20 +0600

    Added db.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-//This is the root App Component
+import { useState, useEffect } from "react"
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
 
-function App() {
-  const name = 'Zubaer'
-  return (
-    <div className="App">
-      <h1>Hello from {name}</h1>
-    </div>
-  );
+//This is the root App Component
+const App = () => {
+   const [showAddTask, setShowAddTask] = useState(false)
+   const [tasks, setTasks] = useState([])
+
+   useEffect(() => {
+      const getTasks = async () => {
+         const tasksFromServer = await fetchTasks()
+         setTasks(tasksFromServer)
+      }
+      getTasks()
+   }, [])
+
+   //Fetch Tasks
+   const fetchTasks = async () => {
+      const res = fetch("http://localhost:5000/tasks")
+      const data = await (await res).json()
+      return data
+   }
+
+   //Add Task
+   const addTask = (task) => {
+      const id = Math.floor(Math.random() * 10000) + 1
+      const newTask = { id, ...task }
+      setTasks([...tasks, newTask])
+   }
+
+   //Delete a Task
+   const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+   }
+
+   //Toggle Reminder
+   const toggleReminder = (id) => {
+      setTasks(
+         tasks.map((task) =>
+            task.id === id ? { ...task, reminder: !task.reminder } : task
+         )
+      )
+   }
+
+   return (
+      <div className="container">
+         <Header
+            title="Task Manager"
+            onAdd={() => setShowAddTask(!showAddTask)}
+            showAdd={showAddTask}
+         />
+         {showAddTask && <AddTask onAdd={addTask} />}
+         {tasks.length > 0 ? (
+            <Tasks
+               tasks={tasks}
+               onDelete={deleteTask}
+               onToggle={toggleReminder}
+            />
+         ) : (
+            "No Tasks to Show"
+         )}
+      </div>
+   )
 }
 
-export default App;
+export default App
